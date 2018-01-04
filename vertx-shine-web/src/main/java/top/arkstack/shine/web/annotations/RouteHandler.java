b11f3a9325ddb8844779150196975dbb22998f17
7le <747037402@qq.com>
2018-01-04 20:13:53 +0800

    zookeeper-address add null pointer process 🏀

--- vertx-shine-web/src/main/java/top/arkstack/shine/web/annotations/RouteHandler.java
+++ vertx-shine-web/src/main/java/top/arkstack/shine/web/annotations/RouteHandler.java
@@ -4,7 +4,14 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
+/**
+ * 通过路径扫描注册
+ * 与 {@link top.arkstack.shine.web.annotations.RouteMapping} 共用
+ * 类似 springmvc 中的@Controller
+ *
+ * @author : 7le
+ * @since v1.0.0
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface RouteHandler {
